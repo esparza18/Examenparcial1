4b66f30178f53d641ee65d81dfc7f8a046a99ad4
Esparza18 <esparzag1416@gmail.com>
2021-09-14 13:58:56 -0500

    insert unidades

--- unidadesvendidas.sql
+++ unidadesvendidas.sql
@@ -1,3 +1,8 @@
 create table unidadesvendidas(
 modelo varchar(100) primary key,
 nombre varchar(100));
+
+insert into unidadesvendidas values('VWCAR2020GOLST', 'GOL 2020');
+insert into unidadesvendidas values('VWCAR2020POLOAUT', 'POLO 2020');
+insert into unidadesvendidas values('VWCAR2020VENTOST', 'VENTO 2020');
+insert into unidadesvendidas values('VWCAR2020JETTAGLI', 'JETTA 2020');
