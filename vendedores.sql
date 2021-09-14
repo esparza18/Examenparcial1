499153a604b7cb992af12d41808a7da961e8b5f2
Esparza18 <esparzag1416@gmail.com>
2021-09-14 13:52:59 -0500

    insert vendedores

--- vendedores.sql
+++ vendedores.sql
@@ -1,3 +1,9 @@
-create table Vendedores(
+create table Vendedores(
 codigo varchar(100) primary key,
 nombre varchar(100));
+
+insert into Vendedores values('VWEM01', 'DE ALBA VILLALOBOS LILIANA LIZBETH');
+insert into Vendedores values('VWEM02', 'FACIO ESPINOSA JUAN CARLOS');
+insert into Vendedores values('VWEM03', 'GOMEZ CANO JOSE MIGUEL');
+insert into Vendedores values('VWEM04', 'GOMEZ CASTAÑEDA MARIA FERNANDA');
+insert into Vendedores values('VWEM05', 'GOMEZ LOPEZ RAFAEL');
